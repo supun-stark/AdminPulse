ff9f27127548e1b734d8b3004a3c25c918d8b133
supun-stark <it19234694@my.sliit.lk>
2024-11-19 20:54:13 +0530

    Initial commit

---
+++ orders.js
@@ -0,0 +1,20 @@
+const Orders = [
+    {
+        productName: 'ReactJs Crash Course',
+        productNumber: 'RJ1212',
+        paymentStatus: 'Due',
+        status: 'Pending'
+    },
+    {
+        productName: 'NextJs Full Course',
+        productNumber: 'NJ1012',
+        paymentStatus: 'Refunded',
+        status: 'Declined'
+    },
+    {
+        productName: 'JavaScript Tutorial',
+        productNumber: 'JS1010',
+        paymentStatus: 'Paid',
+        status: 'Active'
+    },
+]
